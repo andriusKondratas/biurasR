8d97922ffca17d1e6c34d51aa5de0e8cfdb52216
andrius <andrius2kondratas@gmail.com>
2016-04-18 23:45:23 +0300

    cleanup

--- br-0.0.1-SNAPSHOT/src/main/java/ioffice/br/pageflow/common/beans/ApplicationBean.java
+++ br-0.0.1-SNAPSHOT/src/main/java/ioffice/br/pageflow/common/beans/ApplicationBean.java
@@ -3,18 +3,13 @@ package ioffice.br.pageflow.common.beans;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
-
-import ioffice.br.persistance.enums.AuditActionType;
-import ioffice.br.persistance.enums.PersonState;
 import ioffice.br.persistance.model.core.AbstractEntity;
 import ioffice.br.persistance.service.administration.AdministrationObjectCache;
 
@@ -30,47 +25,23 @@ public class ApplicationBean extends BasicBean implements Serializable {
 
 	List<SelectItem> personActivities;
 	List<SelectItem> personStates;
-	List<SelectItem> regions;
-	List<SelectItem> actionTypes;
 	List<SelectItem> yesNo;
-	List<SelectItem> rfmos;
-	List<SelectItem> economicZones;
-	List<SelectItem> countries;
-	List<SelectItem> species;
 	List<SelectItem> years;
-	List<SelectItem> measureUnits;
-	List<SelectItem> limits;
-	List<SelectItem> vesselCategories;
-	
-	private String ttttt;
 
 	public void loadModel() {
 		
 		administrationObjectCache.init();
 		//classificationObjectCache.init();
 		
-		
-		actionTypes = new ArrayList<SelectItem>();
-		for (AuditActionType actionType : AuditActionType.values()) {
-			actionTypes.add(new SelectItem(actionType, getMessage(actionType.getInlLabel())));
-		}
 		yesNo = new ArrayList<SelectItem>();
 		yesNo.add(new SelectItem("",""));
 		yesNo.add(new SelectItem(Boolean.TRUE.toString(), getMessage("common.yes")));
 		yesNo.add(new SelectItem(Boolean.FALSE.toString(), getMessage("common.no")));
 		
-	
-		
-		
-		
 		years = new ArrayList<SelectItem>();
 		for(int i = applicationProperties.getYearListMin(); i <= applicationProperties.getYearListMax(); i++ ){			
 			years.add(new SelectItem(i, String.valueOf(i)));
 		}
-		
-	
-		
-		
 	}
 	
 	private List<SelectItem> getConvertableList(Collection<? extends AbstractEntity> convertables){
@@ -81,28 +52,9 @@ public class ApplicationBean extends BasicBean implements Serializable {
 		return itemList;
 	}
 	
-	public List<SelectItem> getLimits() {
-		return limits;
-	}
-
-	public List<SelectItem> getMeasureUnits() {
-		return measureUnits;
-	}
 
 	public List<SelectItem> getYears() {
 		return years;
-	}
-
-	public List<SelectItem> getSpecies() {
-		return species;
-	}
-
-	public List<SelectItem> getRfmos() {
-		return rfmos;
-	}
-
-	public List<SelectItem> getEconomicZones() {
-		return economicZones;
 	}
 	
 	public List<SelectItem> getPersonActivities() {
@@ -113,24 +65,8 @@ public class ApplicationBean extends BasicBean implements Serializable {
 		return personStates;
 	}
 
-	public List<SelectItem> getRegions() {
-		return regions;
-	}
-
-	public List<SelectItem> getActionTypes() {
-		return actionTypes;
-	}
-
 	public List<SelectItem> getYesNo() {
 		return yesNo;
-	}
-	
-	public List<SelectItem> getCountries() {
-		return countries;
-	}
-
-	public List<SelectItem> getVesselCategories() {
-		return vesselCategories;
 	}
 
 	public AdministrationObjectCache getAdministrationObjectCache() {
@@ -140,11 +76,4 @@ public class ApplicationBean extends BasicBean implements Serializable {
 	public void setAdministrationObjectCache(AdministrationObjectCache administrationObjectCache) {
 		this.administrationObjectCache = administrationObjectCache;
 	}
-
-	public String getTtttt() {
-		return ttttt;
-	}
-
-	
-	
 }
